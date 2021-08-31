b40452520ae0d32d537ba7375e87c1f1d0c02838
KChwistek <vescus@gmail.com>
2021-08-31 21:35:50 +0200

    initial commit - rpi monitor

---
+++ src/main/java/com/raspberrypi/fermzilla/rPiMSMonitor/RPiMsMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.raspberrypi.fermzilla.rPiMSMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RPiMsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RPiMsMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raspberrypi/fermzilla/rPiMSMonitor/RPiMsMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspberrypi.fermzilla.rPiMSMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RPiMsMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
