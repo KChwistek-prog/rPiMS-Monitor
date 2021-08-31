7acdffabf7ab53f5c61ea69f73494674448c1e4a
KChwistek <vescus@gmail.com>
2021-08-31 21:46:27 +0200

    mongodb dependency added, monitor mongodb files moved from rpims-driver

---
+++ src/main/java/com/raspberrypi/fermzilla/rPiMSMonitor/mongodb/BatchRepo.java
@@ -0,0 +1,9 @@
+package com.raspberrypi.fermzilla.rPiMSMonitor.mongodb;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BatchRepo extends MongoRepository<BatchPojo, String> {
+
+}
