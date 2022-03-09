7f69dfafed1025b388ae5766f920166137f3ac1f
Krzysztof <k.chwistek@outlook.com>
2022-03-09 19:47:35 +0100

    removed enablescheldulding from processor

--- src/main/java/com/raspberrypi/fermzilla/rPiMSMonitor/dataProcessor/Processor.java
+++ src/main/java/com/raspberrypi/fermzilla/rPiMSMonitor/dataProcessor/Processor.java
@@ -3,11 +3,9 @@ package com.raspberrypi.fermzilla.rPiMSMonitor.dataProcessor;
 import com.raspberrypi.fermzilla.rPiMSMonitor.mongodb.MongoService;
 import com.raspberrypi.fermzilla.rPiMSMonitor.raspberryPiConnection.PiConnect;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 @Component
-@EnableScheduling
 public class Processor {
     private final PiConnect piConnect;
     private final MongoService mongoService;
